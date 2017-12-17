d0a8e9fad7e55dc0c73c9d0b056b6f2c14d7e685
Tigran Ghabuzyan <tigran.ghabuzyan@gmail.com>
2017-12-17 23:00:16 +0400

    refactoring

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,32 +9,23 @@ import static org.testng.AssertJUnit.assertTrue;
 public class LoginTest {
     private ChromeDriver driver;
 
-    // cuyc enq talis chrome driveri tex@, petq e tanq system property
     @BeforeMethod
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "C:\\dev\\chromedriver.exe");
-        //hajord qayl,chrome driver nor, browser@ bacelu hamar
         driver = new ChromeDriver();
-
     }
-    //nor test enq sarqum URL kanchelu hamar
+
     @Test
     public void login() throws InterruptedException {
         driver.get("http://the-internet.herokuapp.com/login");
-        //run the test, the page should be opened.
-        // log in enq linum
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        // log in button@ push
         driver.findElement(By.cssSelector("#login button")).click();
-        //messagen e cuyc talis
 
-        Thread.sleep(1000); // mi varkyan sleep e linum minchev message@ ga, hetagayum kogtagorcenq "wait", vor@ aveli chisht e
+        Thread.sleep(1000);
         assertTrue( "Login was not succeed!",driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
-
-
     }
-    // should close the browser
+
     @AfterMethod
     public void tearDown() {
         driver.close();
